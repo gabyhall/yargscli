78b563746adfe7576359bd946fb7b0bd77ebfcae
Gaby Hall <gabyhall@Jonathan-Robertss-iMac.local>
2021-07-13 17:35:35 +0100

    add, read, remove

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ const add = () => {
         let stringMovieList = JSON.stringify(movieList.flat(1));
         fs.writeFileSync("./src/netflix.json", stringMovieList);
         console.log(stringMovieList);
+        console.log("added to your movie list");
     }
 }
 
@@ -26,8 +27,28 @@ add();
 
 const list = () => {
   if (command === "list") {
-    console.log(movieList)
+    console.log(movieList);
+    console.log("here is your movie list");
   }
 }
 
 list();
+
+const remove = () => { 
+  if (command === "remove") {
+    entry = {title: yargs.argv.movie, actor: yargs.argv.actor};
+    let deleteIndex;
+    movieList[0].map((movie, index) => {
+      if (movie.title === entry.title) { //note to self: make sure it's entry.title so movie.title isn't always false
+        deleteIndex = index;
+      }
+    });
+    movieList[0].splice(deleteIndex, 1);
+    let stringMovieList = JSON.stringify(movieList.flat(1));
+    fs.writeFileSync("./src/netflix.json", stringMovieList);
+    console.log(movieList)
+  }
+}
+
+remove();
+
